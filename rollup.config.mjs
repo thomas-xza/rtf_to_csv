91a3c589a5477b846468bb21239f3a7ab130d152
mslockin <133052424+mslockin@users.noreply.github.com>
2023-05-10 10:25:54 +0000

    Deploy

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default {
    input: 'src/index.js',
@@ -22,6 +23,7 @@ export default {
       replace({
          preventAssignment: false,
          'process.env.NODE_ENV': '"development"'
-      })
+      }),
+      resolve()
    ]
 }
