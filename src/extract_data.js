37498f98e58fdcb8319579a6f55de85031477f58
mslockin <133052424+mslockin@users.noreply.github.com>
2023-05-10 15:06:49 +0000

    De-monolithed major function into small, functional pieces

--- src/extract_data.js
+++ src/extract_data.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { extract_data_from_rtf } from './extract_data';
+
+
+function extract_data_from_rtf(obj, text_data) {
+    
+    //  this is where the application-specific knowledge is
+
+    // console.log('text-data', typeof text_data, text_data);
+
+    return { ...obj,
+	     
+	     'NAME': extract_forename(data),
+	     
+	     'SURNAME': extract_surname(data),
+	     
+	     'DOB': extract_dob(data),
+	     
+	     'MOB': extract_mobile(data),
+
+	     'TEL': extract_hometel(data),
+
+	     'ADDRESS': extract_addr_line(data),
+
+	     'POST CODE': extract_postcode(data),
+
+	     'REFERRER NAME': extract_refer_name(data),
+
+	     'REFERRING DEPT/ORG': extract_refer_dept(data),
+
+	     'PRACTICE': extract_gp(data) }
+    
+};
 
 
 function extract_names(data) {
@@ -14,104 +44,100 @@ function extract_names(data) {
 
 function extract_forename(data) {
 
-    new_obj['NAME'] = names[1].trim();
+    const names = extract_names(data)
+
+    return names[1].trim();
     
 }
 
 function extract_surname(data) {
 
-    new_obj['SURNAME'] = names[0].trim();
+    const names = extract_names(data)
+    
+    return names[0].trim();
 }
 
 function extract_dob(data) {
 
-    new_obj['DOB'] = text_data.match(/DOB.*Age/)[0].match(/[0-9]{4}/)[0].trim();
+    return data.match(/DOB.*Age/)[0].match(/[0-9]{4}/)[0].trim();
+    
+}
+
+function extract_phones(data) {
+    
+    return data.match(/Telephone.*Hospital/)[0]
+	  .replace('Telephone:','')
+	  .replace('Hospital:','').trim();
+    
 }
 
 function extract_mobile(data) {
 
+    const phones = extract_phones(data)
+
+    return phones.match(/Mobile = [0-9]*/)[0]
+	.replace('Mobile = ','').trim();
+    
 }
 
 function extract_hometel(data) {
 
+    const phones = extract_phones(data)
+
+    return phones.match(/Home = [0-9]*/)[0]
+	.replace('Home = ', '').trim();
 }
 
 function extract_addr(data) {
 
-}
-
-function extract_postcode(data) {
-
-}
-
-function extract_refer_dept(data) {
-
-}
-
-function extract_refer_name(data) {
-
-}
-
-function extract_gp(data) {
-
-}
-
-
-
-function extract_data_from_rtf(new_obj, text_data) {
-    
-    //  this is where the application-specific knowledge is
-
-    // console.log('text-data', typeof text_data, text_data);
-
-    const names = text_data.match(/Name:.*Sex:/)[0]
-	  .replace('Name:','')
-	  .replace('Sex:', '')
-	  .split(',');
-
-    new_obj['NAME'] = names[1].trim();
-    
-    new_obj['SURNAME'] = names[0].trim();
-
-    new_obj['DOB'] = text_data.match(/DOB.*Age/)[0].match(/[0-9]{4}/)[0].trim();
-
-    const phones = text_data.match(/Telephone.*Hospital/)[0]
-	  .replace('Telephone:','')
-	  .replace('Hospital:','').trim();
-
-    new_obj['MOB'] = phones.match(/Mobile = [0-9]*/)[0]
-	.replace('Mobile = ','').trim();
-    
-    new_obj['TEL'] = phones.match(/Home = [0-9]*/)[0]
-	.replace('Home = ', '').trim();
-
-    const address = text_data.match(/Address.*Patient Telephone/)[0]
+    return data.match(/Address.*Patient Telephone/)[0]
 	  .replace('Address:','')
 	  .replace('Patient Telephone','')
 	  .split(',');
 
-    new_obj['ADDRESS'] = address[0].trim();
+}
+
+function extract_addr_line(data) {
+
+    const addr = extract_addr(data)
+
+    return addr[0].trim();
     
-    new_obj['POST CODE'] = address.slice(-1)[0].trim();
+}
 
-    new_obj["REFERRER NAME"] = text_data
-	.match(/Requesting Doctor.*Consultant/)[0]
-	.replace('Requesting Doctor:', '')
-	.replace('Consultant', '')
-	.replace(', ','').trim();
+function extract_postcode(data) {
 
-    new_obj["REFERRING DEPT/ORG"] = text_data
+    const addr = extract_addr(data)
+
+    return addr.slice(-1)[0].trim();
+    
+}
+
+function extract_refer_dept(data) {
+
+    return data
 	.match(/Referring Speciality.*Other/)[0]
 	.replace('Referring Speciality','')
 	.replace('Other','')
 	.replace(',','').trim();
     
-    new_obj["PRACTICE"] = text_data
+}
+
+function extract_refer_name(data) {
+
+    return data
+	.match(/Requesting Doctor.*Consultant/)[0]
+	.replace('Requesting Doctor:', '')
+	.replace('Consultant', '')
+	.replace(', ','').trim();
+
+}
+
+function extract_gp(data) {
+
+    return data
 	.match(/Practice.*_/)[0]
 	.replace('Practice:','')
 	.replace(/,.*/,'').trim();
     
-    return new_obj;
-
-};
-
+}
